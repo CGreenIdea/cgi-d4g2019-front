0e46b80fcdc1161f6ca1b8c84779596a54141765
Fabien HANESSE <fabien.hanesse@dev-abplatform.net>
2019-11-20 18:32:03 +0100

    Removed old css ref in html

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 import '../css/main.scss';
 
 // Here the JS
+class Container extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+      this.innerHTML = '<h1>green L4tern</h1>';
+    }
+}
+
+customElements.define('main-container', Container);
+let container = new Container();
+document.querySelector('body').appendChild(container);
