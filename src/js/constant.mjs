8e761cecece05a4a252746190de9d8bce2e29ee2
Fabien HANESSE <fabien.hanesse@dev-abplatform.net>
2019-11-21 09:49:41 +0100

    removed unused

--- src/js/constant.mjs
+++ src/js/constant.mjs
@@ -2,12 +2,7 @@ console.log("constant");
 
 
 export class Constants {
-
     static get serverBaseUrl() {
         return "http://localhost:8080/";
-    }
-
-    static get constant2() {
-        return constant2;
     }
 }
