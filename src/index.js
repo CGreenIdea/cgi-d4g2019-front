c0828999a53366206101bfc8da3b2d762d4f1eac
Fabien HANESSE <fabien.hanesse@dev-abplatform.net>
2019-11-22 05:19:06 +0100

    Started implementation to remova localhost url

--- src/index.js
+++ src/index.js
@@ -4,46 +4,48 @@ import './js/index';
 // Import stylesheet
 import './css/main.scss';
 
-//import {Constants} from './js/constant.mjs';
-import {callRest} from './js/services.mjs';
+import { callRest } from './js/services.mjs';
 window.callRest = callRest;
 
-import {submitLogin} from './js/login.mjs';
+import { submitLogin } from './js/login.mjs';
 window.submitLogin = submitLogin;
 
+import { correctFormActionUrl } from './js/login.mjs';
+window.correctFormActionUrl = correctFormActionUrl;
+
 //#region Inscription
-import {slideSubscriptionNext} from './js/inscription.mjs';
+import { slideSubscriptionNext } from './js/inscription.mjs';
 window.slideSubscriptionNext = slideSubscriptionNext;
 
-import {slideSubscriptionPrevious} from './js/inscription.mjs';
+import { slideSubscriptionPrevious } from './js/inscription.mjs';
 window.slideSubscriptionPrevious = slideSubscriptionPrevious;
 
-import {checkChangeToOwner} from './js/inscription.mjs';
+import { checkChangeToOwner } from './js/inscription.mjs';
 window.checkChangeToOwner = checkChangeToOwner;
 
-import {submitSubscriptionForm} from './js/inscription.mjs';
+import { submitSubscriptionForm } from './js/inscription.mjs';
 window.submitSubscriptionForm = submitSubscriptionForm;
 
-import {checkDisclaimer} from './js/inscription.mjs';
+import { checkDisclaimer } from './js/inscription.mjs';
 window.checkDisclaimer = checkDisclaimer;
 //#endregion
 
 //#region Documents
-import {generateSVG} from './js/consomation.mjs';
+import { generateSVG } from './js/consomation.mjs';
 window.generateSVG = generateSVG;
 //#endregion
 
 //#region Admin Consultation
-import {CallGetConsultationData} from './js/admin.consultation.mjs';
+import { CallGetConsultationData } from './js/admin.consultation.mjs';
 window.CallGetConsultationData = CallGetConsultationData;
 //#endregion
 
 //#region Admin Consultation
-import {callHousingList} from './js/import.mjs';
+import { callHousingList } from './js/import.mjs';
 window.callHousingList = callHousingList;
 //#endregion
 
 //#region User Documents
-import {callUserDocumentsList} from './js/documents.mjs';
+import { callUserDocumentsList } from './js/documents.mjs';
 window.callUserDocumentsList = callUserDocumentsList;
 //#endregion
--- src/js/login.mjs
+++ src/js/login.mjs
@@ -1,8 +1,8 @@
-export function submitLogin()
-{
+import { Constants } from './constant.mjs';
+
+export function submitLogin() {
     let form = document.getElementById("loginForm");
-    if(form.checkValidity())
-    {
+    if (form.checkValidity()) {
         var dtExpireDel = new Date();
         dtExpireDel.setTime(dtExpireDel.getTime() - 1);
         setCookie('quarkus-credential', '', dtExpireDel, '/');
@@ -10,10 +10,15 @@ export function submitLogin()
     }
 }
 
-function setCookie(nom, valeur, expire, chemin, domaine, securite){
-     document.cookie = nom + ' = ' + escape(valeur) + '  ' +
-               ((expire == undefined) ? '' : ('; expires = ' + expire.toGMTString())) +
-               ((chemin == undefined) ? '' : ('; path = ' + chemin)) +
-               ((domaine == undefined) ? '' : ('; domain = ' + domaine)) +
-               ((securite == true) ? '; secure' : '');
-   }
+export function correctFormActionUrl(formId, url) {
+    const actLink = `${Constants.serverBaseUrl}/${url}`;
+    document.getElementById(formId).action = actLink;
+}
+
+function setCookie(nom, valeur, expire, chemin, domaine, securite) {
+    document.cookie = nom + ' = ' + escape(valeur) + '  ' +
+        ((expire == undefined) ? '' : ('; expires = ' + expire.toGMTString())) +
+        ((chemin == undefined) ? '' : ('; path = ' + chemin)) +
+        ((domaine == undefined) ? '' : ('; domain = ' + domaine)) +
+        ((securite == true) ? '; secure' : '');
+}
