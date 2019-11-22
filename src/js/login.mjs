3f0cff454437e305dc90f20535cdbcd69fd6f45a
cyril.mella <myhpep@gmail.com>
2019-11-22 03:18:42 +0100

    erase cookie on login screen

--- src/js/login.mjs
+++ src/js/login.mjs
@@ -3,6 +3,17 @@ export function submitLogin()
     let form = document.getElementById("loginForm");
     if(form.checkValidity())
     {
+        var dtExpireDel = new Date();
+        dtExpireDel.setTime(dtExpireDel.getTime() - 1);
+        setCookie('quarkus-credential', '', dtExpireDel, '/');
         form.submit();
     }
 }
+
+function setCookie(nom, valeur, expire, chemin, domaine, securite){
+     document.cookie = nom + ' = ' + escape(valeur) + '  ' +
+               ((expire == undefined) ? '' : ('; expires = ' + expire.toGMTString())) +
+               ((chemin == undefined) ? '' : ('; path = ' + chemin)) +
+               ((domaine == undefined) ? '' : ('; domain = ' + domaine)) +
+               ((securite == true) ? '; secure' : '');
+   }
